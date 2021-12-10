b4ec601d3fef8ce56c73d79dd10c965b1c8e030b
3rr0r408 <95542697+3rr0r408@users.noreply.github.com>
2021-12-10 20:18:36 +0100

    Update Buscar_divisores.sh

--- Buscar_divisores.sh
+++ Buscar_divisores.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 read -p "Mete un numero: " n
+
+#Obliga a ingresar un parametro para la ejecucion correcta del programa
 while true
 do
 	if [ -z $n ]
@@ -11,9 +13,12 @@ do
 	fi
 done
 	echo "has ingresado $n"
+	#Variable de comparacion
 	re="^[0-9]+$"
 while true
 do
+	#Compara la variable ingresada y en caso de que este no sea un numero
+        #Cancela la ejecucion 
 	if ! [[ $n =~ $re ]]
 	then
 		echo "No es un numero"
@@ -21,11 +26,14 @@ do
 	else
 		echo "$n es un valor valido"
 	fi
+	#Evita que el vaor ingresado sea menor que 0
 	if [ $n -lt 0 ]
 	then
 		echo "no puede ser menor que 0"
 		break
 	fi
+	#Muestra por pantalla los valores solo si el resto de la division entre
+        # $n/$i es igual a 0, ya que eso significa que ese valor es divisor
 	i=1
 	echo "Los divisores de $n son:"
 	while [ $i -le $n ]
