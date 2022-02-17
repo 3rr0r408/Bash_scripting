3c4a8e538c8ad084b6dd893cf2bb3b11d56204f3
3rr0r408 <95542697+3rr0r408@users.noreply.github.com>
2022-02-17 18:10:11 +0100

    Update Buscar_cadena.sh

--- Buscar_cadena.sh
+++ Buscar_cadena.sh
@@ -19,11 +19,16 @@ do
 		echo "El proceso no puede continuar si lista.txt no esta vacio"
 		break
 	fi
+
+
+
+
 	#Comprobar que se ingresen siempre 2 parametros
 	if [ $# -gt 2 ]
 	then
 		echo "debes ingresar solo 2 parametros"
 		break
+
 	elif [ $# -lt 2 ]
 	then
 		echo "debes ingresar minimo 2 parametros"
@@ -33,6 +38,7 @@ do
 		wrd="$2"
 		sleep 1
 	fi
+
 	#Comprueba que el parametro que indica la ruta del directorio sea correcto
 	if [ -d $1 ]
 	then
@@ -46,24 +52,29 @@ do
 		echo "Debes ingresar una ruta valida"
 		break
 	fi
+
 	#Busca los ficheros dentro de la ruta indicada
 	ficheros=(`find $ruta -type f`)
+
 	#Comprueba si la cadena de texto indicada se encuentra en el fichero
 	#Y si es asi agrega el nombre a listas.txt
+
 	for i in "${ficheros[@]}"
 	do
-		contenido=`cat $i | grep $wrd`
+		contenido=`cat $i | grep "$wrd"`
 		if [ -n "$contenido" ]
 		then
 			echo $i >> lista.txt
 		fi
 	done
+
 	#Indica cuales son los documentos encontrados
 	echo "Los Ficheros cuyo contenido coiciden con $wrd son:"
 	pos=1
 	while read line
 	do
 	echo "$pos: $line"
+	let pos=$pos+1
 	done < lista.txt
 	break
 done
